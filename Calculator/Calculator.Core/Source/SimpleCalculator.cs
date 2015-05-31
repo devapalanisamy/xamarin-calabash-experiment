d20cdfce4e9b15784760fafe0fe54c33bb4e77d7
Deva <dev.palanisamy@gmail.com>
2015-05-31 12:05:38 +0100

    Initial Commit for calabash

---
+++ Calculator/Calculator.Core/Source/SimpleCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator.Core
+{
+	public class SimpleCalculator
+	{
+		public SimpleCalculator ()
+		{
+		}
+			
+		public object Add (int firstNumber, int secondNumber)
+		{
+			return firstNumber + secondNumber;
+		}
+	}
+}
+
---
+++ Calculator/Calculator.Tests/Source/TestSimpleCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using Calculator.Core;
+
+namespace Calculator.Tests
+{
+	[TestFixture()]
+	public class TestSimpleCalculator
+	{
+		private SimpleCalculator simpleCalculator;
+
+		public TestSimpleCalculator ()
+		{
+		}
+
+		[Test()]
+		public void TestAddMethod()
+		{
+			Assert.AreEqual (15, simpleCalculator.Add (10, 5));
+		}
+
+		[SetUp]
+		public void SetUpSimpleCalculator()
+		{
+			simpleCalculator = new SimpleCalculator ();
+		}
+	}
+}
+
